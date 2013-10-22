9e2028304f108a649d9b97c9222a9a0cb38d4c70
ericrius1 <ericrius1@gmail.com>
2013-10-22 15:57:25 -0700

    stars disapearing

--- js/stars.js
+++ js/stars.js
@@ -1,9 +1,10 @@
-var Stars = function(scene){
+var Stars = function(scene) {
 
 	var scene = scene;
 	var starGroup, starEmitter;
 	var maxAge = 5;
 
+	var rand = Math.random;
 	init();
 
 	function init() {
@@ -13,30 +14,37 @@ var Stars = function(scene){
 			blending: THREE.AdditiveBlending
 		});
 
-		starEmitter = new ShaderParticleEmitter({
-			type: "sphere",
-			position: new THREE.Vector3(0, 30, 0),
-			positionSpread: new THREE.Vector3(200, 30, 10),
-			radius: 0.01,
-			radiusScale: new THREE.Vector3(1,1,1),
 
 
-			colorStart: new THREE.Color('white'),
-			colorEnd: new THREE.Color('white'),
-			size: 2,
-			sizeEnd: 2,
-			opacityStart: 0,
-			opacityMiddle: 1,
-			opacityEnd: 0,
+		for (var i = 0; i < 100; i++) {
 
-			particlesPerSecond: 1000
-		});
-		starGroup.addEmitter(starEmitter);
-		scene.add(starGroup.mesh);
+			var xPos = -100 + rand() * 200;
+			var yPos = 20 + rand() * 20;
+	
+			starEmitter = new ShaderParticleEmitter({
+				type: "sphere",
+				position: new THREE.Vector3(xPos, yPos, 0),
+				radius: 0.1,
+
+
+				colorStart: new THREE.Color('white'),
+				colorEnd: new THREE.Color('white'),
+				size: 1,
+				sizeEnd: 1,
+				opacityStart: 0,
+				opacityMiddle: 1,
+				opacityEnd: 0,
+				emitterDuration: Math.random() * 20,
+
+				particlesPerSecond: 100
+			});	
+			starGroup.addEmitter(starEmitter);
+			scene.add(starGroup.mesh);
+		}
 
 	}
 
-	var tick = function(dt){
+	var tick = function(dt) {
 		starGroup.tick(dt);
 	}
 
